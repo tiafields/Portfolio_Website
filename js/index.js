4b60642c8b52e9f51b098a4b84a7acab38ce169c
Tia Fields <tia.fields226@gmail.com>
2024-01-05 15:16:10 -0500

    degbuggint my work section, navigation, and adding nav functionality with scrolling effect

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-const navToggle = document.querySelector('nav.toggle');
+const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const header = document.querySelector('.header');
+const intro = document.querySelector('.intro');
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
@@ -10,3 +13,14 @@ navLinks.forEach(link => {
 
     })
 })
+
+window.addEventListener('scroll', () => {
+    const bottomOfIntro = intro.getBoundingClientRect().height;
+    const headerHeight = header.getBoundingClientRect().height;
+    const threshold = bottomOfIntro - headerHeight;
+    if (window.scrollY >= threshold) {
+        header.classList.add('header--scrolled');
+    } else {
+        header.classList.remove('header--scrolled');
+    }
+});

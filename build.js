a78a04e526b2f6eb4a7c7b44213e02f65b67edb7
Tia Fields <tia.fields226@gmail.comy>
2024-02-17 15:41:36 -0800

    make build command watch

--- build.js
+++ build.js
@@ -2,9 +2,11 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import ejs from 'ejs';
+import chokidar from 'chokidar';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const shouldWatch = process.argv.includes('-w');
 
 const files = [
     'index',
@@ -15,7 +17,19 @@ const files = [
     'portfolio/southwest',
 ];
 
-const main = async () => {
+const cleanHTMLFrom = (dir) => {
+    const oldHTMLFiles = fs.readdirSync(dir);
+    oldHTMLFiles.forEach(file => {
+        if(file.split('.')[1] === 'html') {
+            fs.unlinkSync(path.join(dir, file));
+        }
+    });
+}
+
+const build = async () => {
+    cleanHTMLFrom(path.join(__dirname, 'static'));
+    cleanHTMLFrom(path.join(__dirname, 'static', 'portfolio'));
+
     if (!fs.existsSync(path.join(__dirname, 'static', 'portfolio'))) {
         fs.mkdirSync(path.join(__dirname, 'static', 'portfolio'));
     }
@@ -33,8 +47,40 @@ const main = async () => {
         console.log(`creating file: ${file}.html`);
         fs.writeFileSync(path.join(__dirname, 'static', `${file}.html`), output);
     }
-    
-    process.exit(0);
+};
+
+const main = async () => {
+    if (shouldWatch) {
+        console.log(`Building then watching for changes`);
+        await build();
+        const watcher = chokidar.watch(
+            path.join(__dirname, 'views'),
+            {
+                ignored: /^\./,
+                persistent: true,
+                ignoreInitial: true,
+            }
+        );
+
+        watcher
+            .on('add', file => {
+                console.log(`file added: ${file}`);
+                build();
+            })
+            .on('change', file => {
+                console.log(`file updated: ${file}`);
+                build();
+            })
+            .on('unlink', file => {
+                console.log(`file removed: ${file}`);
+                build();
+            })
+            .on('error', (error) => console.error('Watcher error', error))
+    } else {
+        console.log(`Building`);
+        await build();
+        process.exit(0);
+    }
 };
 
 main();
